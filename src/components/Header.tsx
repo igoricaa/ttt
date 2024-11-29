b33b666ac156a5904f43b0093891e6fa103b4757
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-11-29 13:27:20 +0100

    header logo color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,13 @@ import { routes } from '@/data/data';
 import { Route } from '@/lib/types';
 import NavLink from './ui/nav-link';
 import MobileNav from './mobile-menu';
-import Logo from './ui/logos/logo';
+import HeaderLogo from './ui/logos/header-logo';
 
 const Header = () => {
   return (
     <header className='absolute top-0 left-0 w-full lg:top-5 z-50 flex items-center justify-between sm:justify-end lg:block px-side pt-6 sm:pt-10 lg:pt-0'>
       <div className='lg:hidden sm:absolute sm:left-1/2 sm:-translate-x-1/2'>
-        <Logo
-          color='#4A4B2F'
-          className='h-[38px] w-[34px] sm:h-[60px] sm:w-[53.5px]'
-        />
+        <HeaderLogo className='h-[38px] w-[34px] sm:h-[60px] sm:w-[53.5px]' />
       </div>
       <div className='hidden lg:flex items-center justify-center gap-16'>
         <nav className='flex items-center gap-16'>
@@ -22,7 +19,7 @@ const Header = () => {
           ))}
         </nav>
 
-        <Logo />
+        <HeaderLogo />
 
         <nav className='flex items-center gap-16'>
           {routes.slice(-3).map((route: Route, index: number) => (
---
+++ src/components/ui/logos/header-logo.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Logo from './logo';
+import { useEffect, useState } from 'react';
+
+const pageColors: Record<string, string> = {
+  '/': '#000',
+  '/about-us': '#4A4B2F',
+  '/contact-us': '#4A4B2F',
+  '/memberships': '#4A4B2F',
+  '/programs': '#FFFFFF',
+};
+
+const HeaderLogo = ({ className }: { className?: string }) => {
+  const pathname = usePathname();
+  const [color, setColor] = useState('#000');
+
+  useEffect(() => {
+    setColor(pageColors[pathname]);
+  }, [pathname]);
+
+  return <Logo color={color} className={className} />;
+};
+
+export default HeaderLogo;
