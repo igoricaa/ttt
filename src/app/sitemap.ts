976b6433b0d6d540c1c5a688df95678768f367e3
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-02-11 22:22:36 +0100

    sitemap change

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,114 +1,40 @@
 import { MetadataRoute } from 'next';
 import { routing, getPathname } from '@/i18n/routing';
 
-const baseUrl = process.env.SITE_URL || 'https://thundertopteam.com';
+const baseUrl = 'https://thundertopteam.com';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const staticPages: MetadataRoute.Sitemap = [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          sr: `${baseUrl}/sr`,
-          ru: `${baseUrl}/ru`,
-        },
-      },
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/about-us`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          sr: `${baseUrl}/sr/o-nama`,
-          ru: `${baseUrl}/ru/o-nas`,
-        },
-      },
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/memberships`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          sr: `${baseUrl}/sr/clanovi`,
-          ru: `${baseUrl}/ru/члены`,
-        },
-      },
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/programs`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          sr: `${baseUrl}/sr/programi`,
-          ru: `${baseUrl}/ru/programi`,
-        },
-      },
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/schedule`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          sr: `${baseUrl}/sr/raspored`,
-          ru: `${baseUrl}/ru/raspisanie`,
-        },
-      },
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/contact-us`,
-      lastModified: new Date(),
-      alternates: {
-        languages: {
-          sr: `${baseUrl}/sr/kontakt`,
-          ru: `${baseUrl}/ru/контакт`,
-        },
-      },
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    getEntry('/', 'weekly', 1),
+    getEntry('/about-us', 'monthly', 0.8),
+    getEntry('/memberships', 'monthly', 0.8),
+    getEntry('/programs', 'monthly', 0.8),
+    getEntry('/schedule', 'monthly', 0.8),
+    getEntry('/contact-us', 'monthly', 0.8),
   ];
-
-  return staticPages;
 }
 
 type Href = Parameters<typeof getPathname>[0]['href'];
-function getUrl(href: Href, locale: (typeof routing.locales)[number]) {
-  const pathname = getPathname({ locale, href });
-  console.log('url: ', baseUrl + pathname);
-  return baseUrl + pathname;
+
+function getEntry(
+  href: Href,
+  changeFrequency: 'weekly' | 'monthly',
+  priority: number
+) {
+  return {
+    url: getUrl(href, routing.defaultLocale),
+    alternates: {
+      languages: Object.fromEntries(
+        routing.locales.map((locale) => [locale, getUrl(href, locale)])
+      ),
+    },
+    lastModified: new Date(),
+    changeFrequency: changeFrequency,
+    priority: priority,
+  };
 }
 
-// const baseUrl = 'https://thundertopteam.com';
-
-// export default function sitemap(): MetadataRoute.Sitemap {
-//   return [
-//     getEntry('/'),
-//     getEntry('/about-us'),
-//     getEntry('/memberships'),
-//     getEntry('/programs'),
-//     getEntry('/schedule'),
-//     getEntry('/contact-us'),
-//   ];
-// }
-
-// function getEntry(href: Href) {
-//   return {
-//     url: getUrl(href, routing.defaultLocale),
-//     alternates: {
-//       languages: Object.fromEntries(
-//         routing.locales.map((locale) => [locale, getUrl(href, locale)])
-//       ),
-//     },
-//   };
-// }
+function getUrl(href: Href, locale: (typeof routing.locales)[number]) {
+  const pathname = getPathname({ locale, href });
+  return baseUrl + pathname;
+}
