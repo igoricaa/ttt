11ce2b68661d92af1ef69f81c652ae71814212ae
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-02-11 16:23:14 +0100

    hero and gallery new images

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,51 +1,51 @@
-import Image from 'next/image';
-import heroBg from '@/../public/thunder-top-team-hero.png';
 import Link from '../ui/link-button';
 import { getTranslations } from 'next-intl/server';
-// import { getImageProps } from 'next/image';
+import { getImageProps } from 'next/image';
 
 const Hero = async () => {
   const t = await getTranslations('home.hero');
 
-  // const common = {
-  //   alt: 'Thunder Top Team',
-  //   sizes: '100vw',
-  //   fill: true,
-  //   priority: true,
-  // };
-  // const {
-  //   props: { srcSet: desktop },
-  // } = getImageProps({
-  //   ...common,
-  //   src: '/thunder-top-team-hero.png',
-  // });
-  // const {
-  //   props: { srcSet: mobile, ...rest },
-  // } = getImageProps({
-  //   ...common,
-  //   src: '/thunder-top-team-hero2.jpg',
-  // });
+  const common = {
+    alt: 'Thunder Top Team',
+    sizes: '100vw',
+    fill: true,
+    priority: true,
+  };
+  const {
+    props: { srcSet: desktop },
+  } = getImageProps({
+    ...common,
+    src: '/hero/thunder-top-team-hero.png',
+  });
+  const {
+    props: { srcSet: tablet, ...rest },
+  } = getImageProps({
+    ...common,
+    src: '/hero/thunder-top-team-hero-tablet.png',
+  });
+  const {
+    props: { srcSet: mobile },
+  } = getImageProps({
+    ...common,
+    src: '/hero/thunder-top-team-hero-mobile.png',
+  });
 
   return (
     <section className='grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-12 sm:h-[calc(100vw-12px)] lg:h-[calc(100vh-1.25rem)] relative sm:pb-5 lg:pb-10 px-4 lg:px-side'>
       <div className='clip-path-animation animate-reveal relative sm:absolute col-span-full left-1/2 -translate-x-1/2 w-[calc(100vw-32px)] lg:w-[calc(100vw-2*var(--padding-side))] aspect-square sm:aspect-auto sm:h-full -z-10'>
-        {/* <picture>
-          <source media='(min-width: 1000px)' srcSet={desktop} />
-          <source media='(max-width: 1000px)' srcSet={mobile} />
+        <picture>
+          <source media='(min-width: 1024px)' srcSet={desktop} />
+          <source
+            media='(min-width: 481px) and (max-width: 1023px)'
+            srcSet={tablet}
+          />
+          <source media='(max-width: 480px)' srcSet={mobile} />
           <img
             {...rest}
             style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
           />
-        </picture> */}
+        </picture>
 
-        <Image
-          src={heroBg}
-          alt='Thunder Top Team Hero'
-          fill
-          priority
-          sizes='100vw'
-          style={{ objectFit: 'cover' }}
-        />
         <div className='z-20 absolute bottom-3 sm:hidden'>
           <h1 className='text-3xl sm:text-8xl lg:text-9xl font-bold uppercase'>
             Thunder Top Team
